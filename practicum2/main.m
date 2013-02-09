c71c2cd042e92649c412bb52d771339c3321c943
Joshua <joshua@Joshuas-MacBook-Pro.local>
2013-02-09 15:54:43 -0500

    Initial Commit

---
+++ practicum2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  practicum2
+//
+//  Created by Joshua on 2/9/13.
+//  Copyright (c) 2013 Joshua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
